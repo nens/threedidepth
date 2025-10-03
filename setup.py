877cf16cf0c8740ac444ce3b0a53c36eff0d0792
Arjan Verkerk <arjan.verkerk@gmail.com>
2025-10-03 14:35:56 +0200

    Back to development: 0.7.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import splitext
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.7.4'
+version = '0.7.5.dev0'
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
